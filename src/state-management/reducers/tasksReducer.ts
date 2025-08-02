d3d6eee5b29077b102000ed50c831bd8e583f06a
Amey Gaikar <gaikaramey04@gmail.com>
2025-08-02 12:48:57 +0530

    Creating complex actions

---
+++ src/state-management/reducers/tasksReducer.ts
@@ -0,0 +1,29 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: "ADD";
+  task: Task; // task object to be added.
+}
+
+interface Deletetask {
+  type: "DELETE";
+  taskId: number;
+}
+
+type TaskAction = AddTask | Deletetask;
+
+const tasksReducer = (tasks: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...tasks];
+
+    case "DELETE":
+      return tasks.filter((t) => t.id != action.taskId);
+  }
+};
+
+
+export default tasksReducer
