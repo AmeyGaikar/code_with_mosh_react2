b27da725bbb9f4cf35467cf9664935f830a50d42
Amey Gaikar <gaikaramey04@gmail.com>
2025-08-05 20:09:14 +0530

    Managing application state with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import NavBar from "./state-management/NavBar";
 import TaskList from "./state-management/tasks/TaskList";
 import AuthProvider from "./state-management/auth/AuthProvider";
 import { TasksProvider } from "./state-management/tasks";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <AuthProvider>
       <TasksProvider>
+        <Counter />
         <NavBar />
         <TaskList />
       </TasksProvider>
